44d24abc741186ad698299f11fafbce429fd337e
Adam Lassy <adam.lassy@havas-174.local>
2016-09-20 12:09:31 -0400

    update

--- TrumpWig.ino
+++ TrumpWig.ino
@@ -1,10 +1,9 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
+/* 
+ TrumpWig
 
- modified 8 Nov 2013
- by Scott Fitzgerald
+ Using arduino sweep example:
  http://www.arduino.cc/en/Tutorial/Sweep
+ 
 */
 
 #include <Servo.h>
